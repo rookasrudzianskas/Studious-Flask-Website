de7cd69a6ca49d1a3d0df3125d32b6cee4ac464b
Rokas <rookas.rudzenskas@gmail.com>
2021-04-21 16:42:55 +0300

    Rokas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,3 +14,19 @@ def about():
     title = "About Rokas"
     names = ["John", "Rokas", "Marry", "Wes", "Sally"]
     return render_template("about.html", names=names, title=title)
+
+
+@app.route('/subscribe')
+def subscribe():
+    title = "Subscribe To My Email Newsletter"
+    return render_template("subscribe.html", title=title)
+
+
+@app.route('/form', methods=["POST"])
+def form():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+
+    title = "Thank you!"
+    return render_template("form.html", title=title, first_name=first_name, last_name=last_name, email=email)
