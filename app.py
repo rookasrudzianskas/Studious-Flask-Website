fd52bd8cf0437fe55b6b66c3b374214773e3b2d7
Rokas <rookas.rudzenskas@gmail.com>
2021-04-22 14:46:00 +0300

    Rokas

--- app.py
+++ app.py
@@ -26,9 +26,15 @@ def subscribe():
 
 @app.route('/form', methods=["POST"])
 def form():
+
     first_name = request.form.get("first_name")
     last_name = request.form.get("last_name")
     email = request.form.get("email")
+
+    if not first_name or not last_name or not email:
+        error_statement = "All form fields are required!"
+        return  render_template("subscribe.html", error_statement=error_statement, first_name=first_name, last_name=last_name, email=email)
+
     subscribers.append(f"{first_name} {last_name} | {email}")
 
     title = "Thank you!"
