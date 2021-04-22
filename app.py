89017c8103d782dafb29e0e4056f62ee2654d95f
Rokas <rookas.rudzenskas@gmail.com>
2021-04-22 16:24:53 +0300

    Rokas

--- app.py
+++ app.py
@@ -1,9 +1,64 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import smtplib
+
+# ==========================================================================
 
 app = Flask(__name__)
 
+# ==========================================================================
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
+#  INitialiazi database
+db = SQLAlchemy(app)
+
+
+# ==========================================================================
+
+# Create db model
+
+class Friends(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Create a function to return a string when we add
+
+    def __repr__(self):
+        return '<Name %r>' % self.id
+
+
+# ============================================================================
+
 subscribers = []
+
+
+# ============================================================================
+
+@app.route('/friends', methods=['POST', 'GET'])
+def friends():
+    title = "Rokas Friends list"
+
+    if request.method == "POST":
+        friend_name = request.form['name']
+        new_friend = Friends(name=friend_name)
+
+        #     Push to database
+
+        try:
+            db.session.add(new_friend)
+            db.session.commit()
+            return redirect('/friends')
+        except:
+            return "There was an error adding friends"
+
+    else:
+        friends = Friends.query.order_by(Friends.date_created)
+        return render_template("friends.html", title=title, friends=friends)
+
+
+# ============================================================================
 
 
 @app.route('/')
@@ -27,7 +82,6 @@ def subscribe():
 
 @app.route('/form', methods=["POST"])
 def form():
-
     first_name = request.form.get("first_name")
     last_name = request.form.get("last_name")
     email = request.form.get("email")
@@ -40,7 +94,8 @@ def form():
 
     if not first_name or not last_name or not email:
         error_statement = "All form fields are required!"
-        return  render_template("subscribe.html", error_statement=error_statement, first_name=first_name, last_name=last_name, email=email)
+        return render_template("subscribe.html", error_statement=error_statement, first_name=first_name,
+                               last_name=last_name, email=email)
 
     subscribers.append(f"{first_name} {last_name} | {email}")
 
